862d9ad5c6395484df22e173bf59ed2ba9b6de9e
nikitayakovina <yakovinans@mir-omsk.ru>
2023-05-28 22:30:07 +0600

    добавил вкладки для формы авторизации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorizationComponent } from './modules/components/authorization/authorization.component';
 import { RegistrationComponent } from './modules/components/registration/registration.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthorizationComponent,
-    RegistrationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthorizationComponent, RegistrationComponent],
+  imports: [BrowserModule, AppRoutingModule, TabsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
