304181c5ad56ccc8fa8a0a57c22e1d5ac64ce76c
nikitayakovina <yakovinans@mir-omsk.ru>
2023-05-28 22:06:04 +0600

    добавил компоненты авторизации и регистрации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './modules/components/authorization/authorization.component';
+import { RegistrationComponent } from './modules/components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
