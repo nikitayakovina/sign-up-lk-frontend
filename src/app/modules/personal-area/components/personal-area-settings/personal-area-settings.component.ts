119dba03eda4a29ff49964414052882df31f0723
nikitayakovina <yakovinans@mir-omsk.ru>
2023-09-14 23:08:15 +0600

    + форма настроеек

--- src/app/modules/personal-area/components/personal-area-settings/personal-area-settings.component.ts
+++ src/app/modules/personal-area/components/personal-area-settings/personal-area-settings.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-area-settings',
   templateUrl: './personal-area-settings.component.html',
-  styleUrls: ['./personal-area-settings.component.css']
+  styleUrls: ['./personal-area-settings.component.css'],
 })
 export class PersonalAreaSettingsComponent {
-
+  public formSettings = new FormGroup({
+    fio: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+  });
 }
--- src/app/modules/personal-area/personal-area.module.ts
+++ src/app/modules/personal-area/personal-area.module.ts
@@ -6,9 +6,15 @@ import { WidgetsComponent } from './components/widgets/widgets.component';
 import { PersonalAreaSettingsComponent } from './components/personal-area-settings/personal-area-settings.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [PersonalAreaMainComponent, WidgetsComponent, PersonalAreaSettingsComponent, CalendarComponent],
-  imports: [CommonModule, PersonalAreaRoutingModule, TabsModule],
+  declarations: [
+    PersonalAreaMainComponent,
+    WidgetsComponent,
+    PersonalAreaSettingsComponent,
+    CalendarComponent,
+  ],
+  imports: [CommonModule, PersonalAreaRoutingModule, TabsModule, ReactiveFormsModule],
 })
 export class PersonalAreaModule {}
