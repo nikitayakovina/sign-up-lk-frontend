22f95ba80839e17634b00710327f6fd21ed70593
nikitayakovina <yakovinans@mir-omsk.ru>
2023-09-20 23:14:52 +0600

    запрос данных настроек

--- src/app/modules/personal-area/components/personal-area-settings/personal-area-settings.component.ts
+++ src/app/modules/personal-area/components/personal-area-settings/personal-area-settings.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../../shared/services/auth.service';
 import { PersonalAreaSettingsService } from '../../services/personal-area-settings.service';
 import { IUser } from '../../../../shared/interfaces/user.interface';
-import { logMessages } from '@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild';
 
 @Component({
   selector: 'app-personal-area-settings',
   templateUrl: './personal-area-settings.component.html',
   styleUrls: ['./personal-area-settings.component.css'],
 })
-export class PersonalAreaSettingsComponent {
+export class PersonalAreaSettingsComponent implements OnInit {
+  private currentUser = this.authService.currentUserValue as IUser;
+
   public formSettings = new FormGroup({
     fio: new FormControl('', Validators.required),
     phone: new FormControl('', Validators.required),
@@ -20,12 +21,20 @@ export class PersonalAreaSettingsComponent {
     private personalAreaSettingsService: PersonalAreaSettingsService,
   ) {}
 
-  public submit() {
-    const currentUser = this.authService.currentUserValue as IUser;
+  public ngOnInit() {
+    this.fetchData();
+  }
 
+  public fetchData() {
+    this.personalAreaSettingsService.fetchData(this.currentUser.id).subscribe((result) => {
+      console.log(result);
+    });
+  }
+
+  public submit() {
     this.personalAreaSettingsService
       .submitSettings({
-        userId: currentUser.id,
+        userId: this.currentUser.id,
         searchServiceUserData: {
           workPhoneNumber: '12312312',
         },
--- src/app/modules/personal-area/services/personal-area-settings.service.ts
+++ src/app/modules/personal-area/services/personal-area-settings.service.ts
@@ -3,12 +3,17 @@ import { SettingsService } from '../../../api/open-api/services/settings.service
 import { PostSearchServiceSettingsRequest } from '../../../api/open-api/models/post-search-service-settings-request';
 import { Observable } from 'rxjs';
 import { PostSearchServiceSettingsResponse } from '../../../api/open-api/models/post-search-service-settings-response';
+import { GetSearchServiceSettingsResponse } from '../../../api/open-api/models/get-search-service-settings-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PersonalAreaSettingsService {
   constructor(private settingsService: SettingsService) {}
+
+  public fetchData(userId: string): Observable<GetSearchServiceSettingsResponse> {
+    return this.settingsService.apiSettingsSearchServiceGet({ userId });
+  }
 
   public submitSettings(
     body: PostSearchServiceSettingsRequest,
