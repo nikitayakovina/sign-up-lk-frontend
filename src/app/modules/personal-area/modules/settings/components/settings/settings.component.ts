9634752ac5e7f3216846d41ff161255c1179651c
nikitayakovina <ya-fi-nikita@yandex.ru>
2024-01-04 13:29:04 +0600

    степпер v1

--- src/app/modules/personal-area/modules/settings/components/settings/settings.component.ts
+++ src/app/modules/personal-area/modules/settings/components/settings/settings.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import { AuthService } from '../../../../../../shared/services/auth.service';
 import { PersonalAreaSettingsService } from '../../services/personal-area-settings.service';
-import {Store} from "@ngxs/store";
-import {AddSettings} from "../../../../../../store/actions/settings/settings.actions";
+import {CdkStepper} from "@angular/cdk/stepper";
 
 @Component({
   selector: 'app-settings',
@@ -11,7 +10,24 @@ import {AddSettings} from "../../../../../../store/actions/settings/settings.act
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  @ViewChild('cdkStepper') cdkStepper: CdkStepper;
+
   private currentUser = this.authService.currentUserValue as string;
+
+  public stepsSettingsForm = new FormGroup({
+    steps: new FormArray([
+      new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        workPhoneNumber: new FormControl('', Validators.required),
+        address: new FormControl([''], Validators.required),
+        telegram: new FormControl('', Validators.required),
+      }),
+      new FormGroup({
+        personalPhoneNumber: new FormControl('', Validators.required)
+      })
+    ])
+  });
 
   public formSettings = new FormGroup({
     firstName: new FormControl('', Validators.required),
@@ -23,11 +39,11 @@ export class SettingsComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private personalAreaSettingsService: PersonalAreaSettingsService,
-    private store: Store
   ) {}
 
   public ngOnInit() {
     this.fetchData();
+    console.log(this.cdkStepper)
   }
 
   public fetchData() {
--- src/app/modules/personal-area/modules/settings/settings.module.ts
+++ src/app/modules/personal-area/modules/settings/settings.module.ts
@@ -4,10 +4,12 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { VisibilityOnSiteComponent } from './components/visibility-on-site/visibility-on-site.component';
+import { StepperComponent } from "./components/stepper/stepper.component";
+import { CdkStepperModule } from "@angular/cdk/stepper";
 
 @NgModule({
-  declarations: [SettingsComponent, VisibilityOnSiteComponent],
-  imports: [CommonModule, ReactiveFormsModule, SettingsRoutingModule],
+  declarations: [SettingsComponent, VisibilityOnSiteComponent, StepperComponent],
+  imports: [CommonModule, ReactiveFormsModule, SettingsRoutingModule, CdkStepperModule],
   exports: [SettingsComponent],
 })
 export class SettingsModule {}
